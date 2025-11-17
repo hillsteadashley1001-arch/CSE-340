6e7ce03cb7626396bfb241a17b0a5b39a71f3aa8
hillsteadashley1001-arch <hillsteadashley1001@gmail.com>
2025-11-17 16:54:38 -0700

    W06 Prepare: Data Validation Activity

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -4,14 +4,20 @@ const baseController = {}
 baseController.buildHome = async function(req, res){
   const nav = await utilities.getNav()
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
   res.render("index", {title: "Home", nav})
 =======
+=======
+>>>>>>> Stashed changes
   req.flash("notice",  "This is a flash message.")
   res.render("index", 
     {title: "Home",
     nav,
     errors: null,
   })
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 }
 
