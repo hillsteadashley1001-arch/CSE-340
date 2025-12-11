e5a1d79f13126d1202b88fb346b5782aebe18139
hillsteadashley1001-arch <hillsteadashley1001@gmail.com>
2025-12-11 15:34:16 -0700

    Clean up server.js by removing middleware
    Removed unused middleware and session management code.

--- server.js
+++ server.js
@@ -13,39 +13,6 @@ const expressLayouts = require('express-ejs-layouts')
 require('dotenv').config()
 
 const app = express()
-const static = require("./routes/static")
-const baseController = require("./controllers/baseController")
-const inventoryRoute = require("./routes/inventoryRoute")
-const accountRoute = require("./routes/accountRoute")
-const utilities = require("./utilities/")
-const session = require("express-session")
-const pool = require("./database/")
-const bodyParser = require("body-parser")
-
-
-/* ***********************
- * Middleware
- * ************************/
- app.use(session({
-  store: new (require('connect-pg-simple')(session))({
-    createTableIfMissing: true,
-    pool,
-  }),
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  name: 'sessionId',
-}))
-
-// Express Messages Middleware
-app.use(require('connect-flash')())
-app.use(function(req, res, next){
-  res.locals.messages = require('express-messages')(req, res)
-  next()
-})
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 const staticRoutes = require('./routes/static')
 const baseController = require('./controllers/baseController')
